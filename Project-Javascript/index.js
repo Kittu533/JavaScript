1e39870f3a978ff178f53d8c8c34446a620ec7b5
Ryobi <robiatmaja7@gmail.com>
2024-03-19 00:48:38 +0700

    first commit

--- Project-Javascript/index.js
+++ Project-Javascript/index.js
@@ -1,7 +1,36 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+// // console.log("Menyalakan mesin kopi");
+// // console.log("Menggiling biji kopi");
+// // console.log("Memanaskan air");
+// // console.log("Mencampurkan air dan kopi");
+// // console.log("Menuangkan kopi ke dalam gelas");
+// // console.log("Menuangkan susu ke dalam gelas");
+// // console.log("Kopi Anda sudah siap!");
+
+// // const coffeeStock = require('./state');
+ 
+// // const makeCoffee = (type, miligrams) => {
+// //   if (coffeeStock[type] >= miligrams) {
+// //     console.log("Kopi berhasil dibuat!");
+// //   } else {
+// //     console.log("Biji kopi habis!");
+// //   }
+// // }
+ 
+// // makeCoffee("robusta", 80);
+
+// // const {coffeeStock,isCoffeeMachineReady} = require('./state');
+// // console.log(coffeeStock);
+// // console.log(isCoffeeMachineReady);
+
+// // ES6 MODULE
+// import {coffeeStock as stock ,isCoffeeMachineReady} from "./state.js";  
+
+// // // const displayStock = stock =>{
+// // //         for (const type in stock){
+// // //             console.log(type);
+// // //         }
+// // // }
+// // displayStock(coffeeStock)
+// console.log(stock);
+// console.log(isCoffeeMachineReady);
+
---
+++ Project-Javascript/state.js
@@ -0,0 +1,13 @@
+const coffeeStock = {
+  arabica: 100,
+  robusta: 150,
+  liberica: 200,
+};
+// const isCoffeeMachineReady = true;
+
+// module.exports = {isCoffeeMachineReady,coffeeStock};
+
+//ES6 MODULE
+const isCoffeeMachineReady = true
+
+export {coffeeStock,isCoffeeMachineReady};
